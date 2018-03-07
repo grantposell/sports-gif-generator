998e9a615c3abef008b8d0380f27ebdd81031c81
Grant Posell <grantposell@Grants-MacBook-Pro.local>
2018-03-06 20:12:12 -0600

    Troubleshooting

--- gif.js
+++ gif.js
@@ -1,55 +1,77 @@
 //creating sports array starting 
-var sports = ["Baseball" , "Softball" , "Curling"];
+var sports = ["Baseball", "Softball", "Curling"];
 
 //creating function to render the buttons
 function renderButtons() {
 
 
-    $("#sports-btn").empty();
+  $("#sports-btn").empty();
 
-    // Looping through the array of sports.
-    for (var i = 0; i < sports.length; i++) {
+  // Looping through the array of sports.
+  for (var i = 0; i < sports.length; i++) {
 
-      var a = $("<button>");
-      // Adding a class
-      a.addClass("sports");
-      // Adding a data-attribute with a value of the movie at index i
-      a.attr("data-name", sports[i]);
-      
-      a.text(sports[i]);
-        
-      // Adding the button to the HTML
-      $("#sports-btn").append(a);
-    }
-  //creating an on click event
-  $("#add-sports").on("click", function(event) {
-    // This event keeps the form from trying to submit itself.
-    event.preventDefault();
+    var a = $("<button>");
+    // Adding a class
+    a.addClass("sports");
+    // Adding a data-attribute with a value of the sport at index i
+    a.attr("data-name", sports[i]);
 
-    // This line will grab the text from the input box
-    var sport = $("#sports-input").val().trim();
-    // The sport input from the textbox is then added to our array
-    sports.push(sport);
+    a.text(sports[i]);
 
-    $("#sports-view").on("click", function(){
-        var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=uinmGi53p3SpdSlN664gKCO1L2Id2Tr2&tag=sports"
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-          })
+    // Adding the button to the HTML
+    $("#sports-btn").append(a);
+  };
 
-    
-    
-    // calling renderButtons which handles the processing of our sports array
-    renderButtons();
+//creating an on click event
+$("#sports-view").on("click", function (event) {
+  // This event keeps the form from trying to submit itself.
+  event.preventDefault();
 
- 
+  // This line will grab the text from the input box
+  var sport = $("#sports-input").val().trim();
+  // The sport input from the textbox is then added to our array
+  sports.push(sport);
 
-  });
-
-  // Calling the renderButtons function at least once to display the initial list of sports
+  // calling renderButtons which handles the processing of our sports array
   renderButtons();
 
+});
+
+// Calling the renderButtons function at least once to display the initial list of sports
+renderButtons(); 
+};
+
+$("#sports-btn").on("click", function () {
+
+  // Storage for giphy API for sports
+  var sport = $(this).attr("data-name");
+  var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=uinmGi53p3SpdSlN664gKCO1L2Id2Tr2&tag=" + sport;
+
+  //  AJAX GET request 
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+
+    
+    .then(function (response) {
+
+      // Saving the image_original_url property
+      var imageUrl = response.data.image_original_url;
+
+      // Creating and storing an image tag
+      var sportsImage = $("<img>");
+
+      sportsImage.attr("src", imageUrl);
+      sportsImage.attr("alt", "sports image");
 
 
-  
+      $("#sports-view").prepend(sportsImage);
+    });
+});
+
+
+
+
+
+
